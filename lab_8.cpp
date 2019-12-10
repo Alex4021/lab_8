96c7d19a7a1fb357870b27fd84fff24699cb2dbd
gento <gento@DESKTOP-IN3HC27>
2019-12-10 03:24:25 +0300

    twinpeaks

--- lab_8.cpp
+++ lab_8.cpp
@@ -2,10 +2,36 @@
 //
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main()
 {
-    
+	int a[3][3];
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			a[i][j] = j+1;
+		}
+	}
+	for (int i = 0; i < 3; i++)  
+	{
+		for (int j = 0; j < 3; j++) 
+		{
+			cout << setw(4) << a[i][j];
+		}
+		cout << endl;
+	}
+	int s;
+	s = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			s += a[i][j];
+		}
+	}
+	cout << "s=" << s;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
